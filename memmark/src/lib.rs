108aef32839ad97467415e035168f457c879dd8e
Chris Wailes <chriswailes@google.com>
2025-05-17 15:27:46 -0700

    Add the MemMark crate
    This crate will host the LibApp used for evaluating memory
    characteristics of the Zygote architecture.
    
    Test: m libmemmark
    Test: m zygote_all
    Bug: 415212489
    Change-Id: I2510273a02f04cdf0d5d0c15b8b412fa56031be1

---
+++ memmark/src/lib.rs
@@ -0,0 +1,25 @@
+//
+// Copyright (C) 2025 The Android Open-Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A library Zygote "application" used to test memory interactions between the
+//! Zygote and its child processes.
+
+/// Entry point for the MemMark Zygote LibApp.
+#[no_mangle]
+pub fn zygote_entry() {
+    print!("Hello from MemMark!");
+
+    std::process::exit(0);
+}
