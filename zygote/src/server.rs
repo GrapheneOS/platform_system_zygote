ed67d74f883992dad43805282905a4ad59e7893a
James Farrell <jamesfarrell@google.com>
2025-08-11 22:10:07 +0000

    Clippy lint fixes for rustc 1.88.0
    Test: m rustClippyJson
    Bug: 435702100
    Flag: EXEMPT refactor
    Change-Id: Ifc7cbcc2031045d2e96a5b243ace36f2c385b9fe

--- zygote/src/server.rs
+++ zygote/src/server.rs
@@ -601,22 +601,20 @@ impl Server {
                     error!("`set_tid` value already exists in the current namespace");
                 } else if errno.is(libc::EINVAL) {
                     error!(
-                        "Invalid argument combination to `clone3()` (see man page for details): {:?}",
-                        clone_args
+                        "Invalid argument combination to `clone3()` (see man page for details): {clone_args:?}"
                     );
                 } else if errno.is(libc::ENOMEM) {
                     error!("Cannot allocate sufficient memory for a new process");
                 } else if errno.is(libc::ENOSPC) {
                     error!(
-                        "Either CLONE_NEWPID or CLONE_NEWUSER were specified and the resulting number of nested namespaces would exceed the maximum allowed depth: {:?}",
-                        clone_args);
+                        "Either CLONE_NEWPID or CLONE_NEWUSER were specified and the resulting number of nested namespaces would exceed the maximum allowed depth: {clone_args:?}");
                 } else if errno.is(libc::EOPNOTSUPP) {
                     // TODO: Print the actual cgroup path once it is present in the spawn params.
                     error!("Destination version 2 cgroup is currently in a domain invalid state: <TODO>");
                 } else if errno.is(libc::EPERM) {
-                    error!("Server lacks the correct permissions to clone with the provided arguments: {:?}", clone_args);
+                    error!("Server lacks the correct permissions to clone with the provided arguments: {clone_args:?}");
                 } else {
-                    error!("Unexpected error code returned by call to `clone3()`: {}", errno);
+                    error!("Unexpected error code returned by call to `clone3()`: {errno}");
                 }
 
                 Break(ClientLoopControl::Error(fd, errno.into()))
