89c78558c4a08d1a51d877472790367e781ae76e
Joe Hattori <hattorij@google.com>
2025-08-15 01:25:43 -0700

    Clean up messages.rs with proc-macro
    Introduces a new `zygote-proc-macros` crate to automate the generation
    of `MarshalParcel` and `UnmarshalParcel` implementations for Zygote
    messages. This replaces the manual implementation of these traits,
    reducing boilerplate and improving maintainability.
    
    The `#[derive(MarshalParcel)]`, `#[derive(UnmarshalParcel)]`, and
    `#[derive(FlattenParcel)]` macros are used in `zygote/src/messages.rs`
    to generate the necessary code for converting between Rust structs/enums
    and FlatBuffers parcels. Custom marshalling/unmarshalling logic is
    handled via attributes like `#[marshal(map = ...)]` and `#[unmarshal(map
    = ...)]`.
    
    Bug: 436727598
    Test: build passes
    Change-Id: Ibcc742ce69196d3696f02437c9473113914a7485

---
+++ zygote-proc-macros/src/marshal.rs
@@ -0,0 +1,271 @@
+//
+// Copyright (C) 2025 The Android Open-Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Attribute, Data, DeriveInput, Error, Expr, Field, Fields, Ident, Variant};
+
+use crate::utils::{get_flatten_into_ident, get_inner_type_ident};
+
+// Generates the MarshalParcel trait implementation for the enum.
+pub(crate) fn gen_marshal_parcel(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let variants = match &ast.data {
+        Data::Enum(data) => &data.variants,
+        _ => {
+            return Error::new(name.span(), "Parcelable can only be derived for enums")
+                .to_compile_error()
+        }
+    };
+
+    let inner_type_arms = variants.iter().map(|v| {
+        let variant_id = &v.ident;
+        let inner_type_id = get_inner_type_ident(v);
+
+        let pattern = match &v.fields {
+            Fields::Unit => quote! {},
+            Fields::Named(_) => quote! { {..} },
+            Fields::Unnamed(_) => quote! { (..) },
+        };
+
+        quote! { #name::#variant_id #pattern => inner::#name::#inner_type_id }
+    });
+
+    let marshal_arms = variants.iter().map(|v| gen_marshal_arm(v, name));
+
+    quote! {
+        impl #impl_generics MarshalParcel<inner::#name> for #name #ty_generics #where_clause {
+            fn inner_type(&self) -> inner::#name {
+                match self {
+                    #(#inner_type_arms),*
+                }
+            }
+
+            fn marshal(
+                &self,
+                builder: &mut flatbuffers::FlatBufferBuilder<'_>,
+            ) -> flatbuffers::WIPOffset<flatbuffers::UnionWIPOffset> {
+                match self {
+                    #(#marshal_arms),*
+                }
+            }
+        }
+    }
+}
+
+// Generates the match arms for the marshel() implementation
+fn gen_marshal_arm(variant: &Variant, enum_name: &Ident) -> TokenStream {
+    let variant_id = &variant.ident;
+    let inner_type_id = get_inner_type_ident(variant);
+    let inner_type_arg_id = Ident::new(&format!("{}Args", inner_type_id), inner_type_id.span());
+
+    match &variant.fields {
+        Fields::Unit => quote! {
+            #enum_name::#variant_id => {
+                inner::#inner_type_id::create(
+                    builder, &inner::#inner_type_arg_id {}).as_union_value()
+            }
+        },
+        Fields::Named(fields) => {
+            let inner_type_args_name = quote! { inner_type_args };
+            let assignments = fields.named.iter().map(|field| {
+                let field_name = field.ident.as_ref().unwrap();
+                gen_field_assignment(
+                    &inner_type_args_name,
+                    field,
+                    &quote! { #field_name },
+                    &quote! { builder },
+                )
+            });
+            let field_names = fields.named.iter().map(|f| f.ident.as_ref().unwrap());
+            quote! {
+                #enum_name::#variant_id { #(#field_names),* } => {
+                    let mut #inner_type_args_name = inner::#inner_type_arg_id::default();
+                    #(#assignments);*;
+                    inner::#inner_type_id::create(builder, &#inner_type_args_name).as_union_value()
+                }
+            }
+        }
+        Fields::Unnamed(_) => quote! {
+            #enum_name::#variant_id(..) => {
+                return Error::new(
+                    variant_id.span(), "Cannot marshal unnamed fields").to_compile_error();
+            }
+        },
+    }
+}
+
+// Geneartes an internal flatten() method.
+// This method takes a parameter `args` and sets field values to it.
+// For example,
+// ```
+// #[derive(FlattenParcel)]
+// #[flatten_into_type = "Inner"]
+// struct Outer {
+//   field1: i32,
+//   field2: i32,
+// }
+// ```
+// will generate something like the following code
+// ```
+// impl Outer {
+//   fn flatten<'a>(&self,
+//                  args: &mut inner::InnerArgs<'a>,
+//                  builder: &mut flatbuffers::FlatBufferBuilder<'a>) {
+//     args.field1 = self.field1;
+//     args.field2 = self.field2;
+//   }
+// }
+// ```
+// flatten() method will be called inside `marshal()` when a field has an attribute #[flatten]
+pub(crate) fn gen_flatten_marshal_parcel(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let flatten_into = get_flatten_into_ident(&ast.attrs);
+    let flatten_into_args = quote::format_ident!("{}Args", flatten_into);
+    let fields_named = match &ast.data {
+        Data::Struct(s) => match &s.fields {
+            Fields::Named(f) => f,
+            _ => {
+                return Error::new(
+                    name.span(),
+                    "FlattenParcel can only be derived for structs with named fields.",
+                )
+                .to_compile_error()
+            }
+        },
+        _ => {
+            return Error::new(name.span(), "FlattenParcel can only be derived for structs.")
+                .to_compile_error()
+        }
+    };
+
+    let assignments = fields_named.named.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        gen_field_assignment(
+            &quote! { args },
+            field,
+            &quote! { self.#field_name },
+            &quote! { builder },
+        )
+    });
+
+    quote! {
+        impl #name {
+            fn flatten<'a>(
+                    &self,
+                    args: &mut inner::#flatten_into_args<'a>,
+                    builder: &mut flatbuffers::FlatBufferBuilder<'a>) {
+                #(#assignments);*;
+            }
+        }
+    }
+}
+
+// Stores a value to the field #field_name of the given #args, depending on #[marshal(..)].
+fn gen_field_assignment(
+    args: &TokenStream,
+    field: &Field,
+    field_access: &TokenStream,
+    builder: &TokenStream,
+) -> TokenStream {
+    let field_name = field.ident.as_ref().unwrap();
+    match MarshalAttr::new(field) {
+        Some(MarshalAttr::Default(lit)) => {
+            quote! { #args.#field_name = #field_access.unwrap_or(#lit) }
+        }
+        Some(MarshalAttr::Packed) => {
+            quote! { #args.#field_name = Some(#field_access.to_packed(#builder)) }
+        }
+        Some(MarshalAttr::Union) => {
+            let type_field_name = quote::format_ident!("{}_type", field_name);
+            quote! {
+                #args.#type_field_name = #field_access.inner_type();
+                #args.#field_name = Some(#field_access.marshal(#builder))
+            }
+        }
+        Some(MarshalAttr::Flatten) => {
+            quote! { #field_access.flatten(&mut #args, #builder) }
+        }
+        Some(MarshalAttr::Map(map)) => {
+            quote! { #args.#field_name = #map(&#field_access, #builder) }
+        }
+        None => quote! { #args.#field_name = *#field_access },
+    }
+}
+
+// Parses #[marshal(..)] attributes.
+enum MarshalAttr {
+    // #[marshal(default = Expr)]
+    // Expects an Option value to be passed, and sets the default value when serializing.
+    Default(Box<Expr>),
+    // #[flatten]
+    // Calls the flatten() defined in gen_flatten_marshal_parcel() when serializing.
+    // This attribute is also used in UnmarshalParcel.
+    Flatten,
+    // #[marshal(packed)]
+    // Expects a value with a type that implements ToPacked trait, and calls to_packed() when
+    // serializing.
+    Packed,
+    // #[union]
+    // Treat the flatbuffer's type as union, which requires another field `<field_name>_type`,
+    // indicating the variant. This attribute is also used in UnmarshalParcel.
+    Union,
+    // #[marshal(map = <fn>)]
+    // Maps a value when serializing based on the given function.
+    Map(Ident),
+}
+
+impl MarshalAttr {
+    fn new(field: &Field) -> Option<Self> {
+        field.attrs.iter().find_map(Self::from_attr)
+    }
+
+    fn from_attr(attr: &Attribute) -> Option<Self> {
+        if attr.path().is_ident("flatten") {
+            return Some(Self::Flatten);
+        }
+        if attr.path().is_ident("union") {
+            return Some(Self::Union);
+        }
+
+        if !attr.path().is_ident("marshal") {
+            return None;
+        }
+
+        let mut ret = None::<Self>;
+
+        attr.parse_nested_meta(|meta| {
+            if meta.path.is_ident("packed") {
+                ret = Some(Self::Packed);
+                Ok(())
+            } else if meta.path.is_ident("default") {
+                let lit = meta.value()?.parse::<Expr>()?;
+                ret = Some(Self::Default(Box::new(lit)));
+                Ok(())
+            } else if meta.path.is_ident("map") {
+                let map = meta.value()?.parse::<Ident>()?;
+                ret = Some(Self::Map(map));
+                Ok(())
+            } else {
+                Err(meta.error("unsupported marshal attribute"))
+            }
+        })
+        .expect("error parsing marshal attribute");
+
+        ret
+    }
+}
